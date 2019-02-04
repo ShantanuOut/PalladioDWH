26d86689931c49b170c1a0f8d00f11b35af35844
Shantanu Narale <shantanunarale062@outlook.com>
2019-02-04 09:21:52 +0530

    Initial Reports

---
+++ Palladio DWH/Palladio DWH/dbo/Stored Procedures/rptGetQuotedCost.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[rptGetQuotedCost]
+	@paramMain		BIT,
+	@paramAgrCst	BIT,
+	@paramTDSchr	BIT
+AS
+BEGIN
+	SELECT
+		dpt.PaymentType,
+		fqc.Amount
+	FROM dbo.factQotedCost fqc
+	INNER JOIN dbo.dimPaymentType dpt
+		ON fqc.PaymentTypeId = dpt.PaymentTypeId	
+	WHERE dpt.PaymentType <> CASE WHEN @paramMain = 0
+								THEN 'Maintainance Cost'
+								ELSE ''
+							 END
+	AND dpt.PaymentType <> CASE WHEN @paramAgrCst = 0
+								THEN 'Agreement Registration'
+								ELSE ''
+							END
+	AND dpt.PaymentType <> CASE WHEN  @paramTDSchr = 0
+								THEN 'TDS'
+								ELSE ''
+							END
+END
